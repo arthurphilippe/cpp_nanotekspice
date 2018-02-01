b7cb00e01fa9565da8f38a59470d08beda53d595
Anthony <anthony.tram@epitech.eu>
2018-02-01 10:38:30 +0100

    #1 Done

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,20 @@
 
 int main(int ac, char **av)
 {
+	std::list<std::unique_ptr<nts::IComponent>> list;
+	nts::IComponent *tmp;
 	if (ac > 1)
 	{
 		try
 		{
 			nts::Parser kappa(av[1]);
+
 		        kappa.readFile(av[1]);
+			list = std::move(kappa.getList());
+			for (auto i = list.begin(); i != list.end(); i++) {
+				tmp = i->get();
+				tmp->dump();
+			}
 		}
 		catch (const FileError &error)
 		{
