29004d53f2641d50e6376e62182ce7fbedd8296b
Anthony <anthony.tram@epitech.eu>
2018-02-04 14:37:57 +0100

    #25 Added some test for Args

--- tests/test-Args.cpp
+++ tests/test-Args.cpp
@@ -11,8 +11,72 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wwrite-strings"
+
+Test(NoArgs, Arguments) {
+	std::cout << "\033[1;31m:: Testing 'No Argument'\033[0m" << std::endl;
+	bool thrown = false;
+        char *str[4] = {"kappa", "tests/counter.nts",
+			"",
+			NULL
+	};
+        try
+	{
+		parserTester(2, str);
+	}
+	catch (const FileError &error)
+	{
+		error.what();
+		thrown = true;
+	}
+	cr_assert(thrown);
+	std::cout << "\033[1;31m--> finished test 'No Argument'\n\033[0m" << std::endl;
+}
+
+
+Test(FalseNameWithValue, Arguments) {
+	std::cout << "\033[1;31m:: Testing 'False Input Name with a True Value'\033[0m" << std::endl;
+	bool thrown = false;
+        char *str[4] = {"kappa", "tests/counter.nts",
+			"prout=0",
+			NULL
+	};
+        try
+	{
+		parserTester(3, str);
+	}
+	catch (const FileError &error)
+	{
+		error.what();
+		thrown = true;
+	}
+	cr_assert(thrown);
+	std::cout << "\033[1;31m--> finished test 'False Input Name with a True Value'\033[0m\n" << std::endl;
+}
+
+Test(FalseName, Arguments) {
+	std::cout << "\033[1;31m:: Testing 'False Input Name without Value'\033[0m" << std::endl;
+	bool thrown = false;
+        char *str[4] = {"kappa", "tests/counter.nts",
+			"prout",
+			NULL
+	};
+        try
+	{
+		parserTester(3, str);
+	}
+	catch (const FileError &error)
+	{
+		error.what();
+		thrown = true;
+	}
+	cr_assert(thrown);
+	std::cout << "\033[1;31m--> finished test 'False Input Name without Value'\n\033[0m" << std::endl;
+}
+
+
+
 Test(RealFile, Arguments) {
-	std::cout << "-------------------TEST REAL FILE" << std::endl;
+	std::cout << "\033[1;31m:: Testing 'Real File with Good Argument'\033[0m" << std::endl;
 	bool thrown = false;
         char *str[4] = {"kappa", "tests/counter.nts",
 			"reset=1",
@@ -28,11 +92,11 @@ Test(RealFile, Arguments) {
 		thrown = true;
 	}
 	cr_assert(!thrown);
-	std::cout << "-------------------END TEST REAL FILE\n" << std::endl;
+	std::cout << "\033[1;31m--> finished test 'Real File with Good Argument'\033[0m\n" << std::endl;
 }
 
 Test(FalseFile, Arguments) {
-	std::cout << "-------------------TEST FALSE FILE" << std::endl;
+	std::cout << "\033[1;31m:: Testing 'False Configuration File with Good Argument'\033[0m" << std::endl;
 	bool thrown = false;
         char *str[3] = {"kappa", "u_u", "o1=1"};
         try
@@ -45,7 +109,7 @@ Test(FalseFile, Arguments) {
 		thrown = true;
 	}
 	cr_assert(thrown);
-	std::cout << "-------------------END TEST FALSE FILE\n" << std::endl;
+		std::cout << "\033[1;31m--> finished test 'False Configuration File with Good Argument'\033[0m\n" << std::endl;
 }
 
 #pragma GCC diagnostic pop
