77cc03ec6e01ff7f82a2d280c379f5646e9e909f
Anthony <anthony.tram@epitech.eu>
2018-02-04 15:03:06 +0100

    #25 Finished Unit Test for Arguments

--- tests/test-Args.cpp
+++ tests/test-Args.cpp
@@ -12,6 +12,27 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wwrite-strings"
 
+Test(RealFileWithImpossibleValue, Arguments) {
+	std::cout << "\033[1;31m:: Testing 'Real File with Impossible Value'\033[0m" << std::endl;
+	bool thrown = false;
+        char *str[4] = {"kappa", "tests/counter.nts",
+			"reset=allah",
+			NULL
+	};
+        try
+	{
+		parserTester(3, str);
+	}
+	catch (const FileError &error)
+	{
+		error.what();
+		thrown = true;
+	}
+	cr_assert(thrown);
+	std::cout << "\033[1;31m--> finished test 'Real File with Impossible Value'\033[0m\n" << std::endl;
+}
+
+
 Test(NoArgs, Arguments) {
 	std::cout << "\033[1;31m:: Testing 'No Argument'\033[0m" << std::endl;
 	bool thrown = false;
@@ -53,6 +74,48 @@ Test(FalseNameWithValue, Arguments) {
 	std::cout << "\033[1;31m--> finished test 'False Input Name with a True Value'\033[0m\n" << std::endl;
 }
 
+
+Test(NoArgumentAtAll, Arguments) {
+	std::cout << "\033[1;31m:: Testing 'No Argument At All'\033[0m" << std::endl;
+	bool thrown = false;
+        char *str[2] = {"kappa",
+			NULL
+	};
+        try
+	{
+		parserTester(1, str);
+	}
+	catch (const FileError &error)
+	{
+		error.what();
+		thrown = true;
+	}
+	cr_assert(thrown);
+	std::cout << "\033[1;31m--> finished test 'No Argument At All'\n\033[0m" << std::endl;
+}
+
+
+Test(RealNameWithEgal, Arguments) {
+	std::cout << "\033[1;31m:: Testing 'Real Input Name with a < = > '\033[0m" << std::endl;
+	bool thrown = false;
+        char *str[4] = {"kappa", "tests/counter.nts",
+			"reset=",
+			NULL
+	};
+        try
+	{
+		parserTester(3, str);
+	}
+	catch (const FileError &error)
+	{
+		error.what();
+		thrown = true;
+	}
+	cr_assert(thrown);
+	std::cout << "\033[1;31m--> finished test 'False Input Name without Value'\n\033[0m" << std::endl;
+}
+
+
 Test(FalseName, Arguments) {
 	std::cout << "\033[1;31m:: Testing 'False Input Name without Value'\033[0m" << std::endl;
 	bool thrown = false;
@@ -72,7 +135,6 @@ Test(FalseName, Arguments) {
 	cr_assert(thrown);
 	std::cout << "\033[1;31m--> finished test 'False Input Name without Value'\n\033[0m" << std::endl;
 }
-
 
 
 Test(RealFile, Arguments) {
@@ -113,3 +175,4 @@ Test(FalseFile, Arguments) {
 }
 
 #pragma GCC diagnostic pop
+
