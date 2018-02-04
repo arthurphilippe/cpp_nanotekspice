53e3666e9d071c99a16699eb771edfe401712d8c
Arthur Philippe <arthur.philippe@epitech.eu>
2018-02-04 15:20:08 +0100

    4013 truthtable

--- include/components/Ref4013Comp.hpp
+++ include/components/Ref4013Comp.hpp
@@ -8,6 +8,7 @@
 #ifndef REF4013COMP_HPP_
 	#define REF4013COMP_HPP_
 
+	#include <list>
 	#include "DefaultComponent.hpp"
 
 namespace nts {
@@ -15,8 +16,23 @@ namespace nts {
 	public:
 		Ref4013Comp(const std::string &name);
 		virtual Tristate compute(std::size_t pin = 1) override;
-	protected:
 	private:
+		struct TruthStatement {
+			Tristate change;
+			Tristate d;
+			Tristate r;
+			Tristate s;
+			Tristate q;
+		};
+
+		Tristate _previous1;
+		Tristate _previous2;
+		Tristate _data1;
+		Tristate _data2;
+		Tristate _currPrev;
+		std::list<TruthStatement> _truthTable;
+		Tristate flipFlop(Tristate clock, Tristate d,
+			Tristate r, Tristate s);
 	};
 }
 
--- src/components/Ref4013Comp.cpp
+++ src/components/Ref4013Comp.cpp
@@ -10,6 +10,23 @@
 nts::Ref4013Comp::Ref4013Comp(const std::string &name)
 {
 	_name.assign(name);
+	_truthTable.push_back({TRUE, FALSE, FALSE, FALSE, FALSE});
+	_truthTable.push_back({TRUE, TRUE, FALSE, FALSE, TRUE});
+	_truthTable.push_back({FALSE, UNDEFINED, FALSE, FALSE, UNDEFINED});
+	_truthTable.push_back({UNDEFINED, UNDEFINED, TRUE, FALSE, TRUE});
+	_truthTable.push_back({UNDEFINED, UNDEFINED, FALSE, TRUE, FALSE});
+	_truthTable.push_back({UNDEFINED, UNDEFINED, TRUE, TRUE, TRUE});
+}
+
+nts::Tristate nts::Ref4013Comp::flipFlop(Tristate clock, Tristate d,
+	Tristate r, Tristate s)
+{
+	auto it = _truthTable.begin();
+
+	while (it != _truthTable.end()) {
+		if (r == it->r && s == it->s);
+		++it;
+	}
 }
 
 nts::Tristate nts::Ref4013Comp::compute(std::size_t pin)
