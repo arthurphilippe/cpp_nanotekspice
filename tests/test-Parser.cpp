7df1fd0315146c3ff28a0ab042d188737761f359
Anthony <anthony.tram@epitech.eu>
2018-02-05 16:28:12 +0100

    #24 Added one test for Parser

--- tests/test-Parser.cpp
+++ tests/test-Parser.cpp
@@ -21,7 +21,25 @@ Test(sampleTest, Parser) {
 Test(EmptyFile, Parser) {
 	std::cout << "\033[1;31m:: Testing 'Empty Configuration File'\033[0m" << std::endl;
 	bool verify = false;
-	char *str[4] = {"nanotekspice", "tests/nts_files/empty.nts", "a=2", NULL};
+	char *str[4] = {"nanotekspice", "tests/nts_files/empty.nts", "a=1", NULL};
+	try {
+	nts::Parser(3, str);
+	} catch (const FileError &error) {
+		verify = true;
+		error.what();
+	}
+	cr_assert(verify);
+	std::cout << "\033[1;31m--> finished test 'Empty Configuration File'\n\033[0m" << std::endl;
+}
+
+
+/*
+**	TO CHANGE
+*/
+Test(EmptyFile2, Parser) {
+	std::cout << "\033[1;31m:: Testing 'Empty Configuration File'\033[0m" << std::endl;
+	bool verify = false;
+	char *str[4] = {"nanotekspice", "tests/nts_files/empty.nts", "a=1", NULL};
 	try {
 	nts::Parser(3, str);
 	} catch (const FileError &error) {
