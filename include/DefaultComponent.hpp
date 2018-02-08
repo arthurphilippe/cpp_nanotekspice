2d51a92951be1a92f3403c56eefb7e31d715165a
Arthur Philippe <arthur.philippe@epitech.eu>
2018-02-08 18:38:38 +0100

    Creation of facoty members and of the genesisMap.
    They are not yet in use since they are not complete.

--- include/DefaultComponent.hpp
+++ include/DefaultComponent.hpp
@@ -11,6 +11,8 @@
 	#include <vector>
 	#include <string>
 	#include <memory>
+	#include <unordered_map>
+	#include <functional>
 	#include "IComponent.hpp"
 
 namespace nts {
@@ -32,6 +34,51 @@ namespace nts {
 		std::string			_name;
 		std::string			_type;
 		std::vector<ComponentLink>	_links;
+	private:
+		static std::unordered_map<std::string,
+			std::function<std::unique_ptr<IComponent>(
+				const std::string &)>> _genesisMap;
+		static void populateGenesisMap();
+		static std::unique_ptr<IComponent> create2716(
+			const std::string &name,
+			const std::string &value);
+		static std::unique_ptr<IComponent> create4001(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4008(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4011(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4013(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4017(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4030(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4040(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4069(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4071(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4081(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4094(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4514(
+			const std::string &name);
+		static std::unique_ptr<IComponent> create4801(
+			const std::string &name);
+		static std::unique_ptr<IComponent> createTrue(
+			const std::string &name);
+		static std::unique_ptr<IComponent> createFalse(
+			const std::string &name);
+		static std::unique_ptr<IComponent> createInput(
+			const std::string &name);
+		static std::unique_ptr<IComponent> createOutput(
+			const std::string &name);
+		static std::unique_ptr<IComponent> createClock(
+			const std::string &name);
+
 	};
 }
 
