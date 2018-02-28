1063ad165ffa257cada4f4f6ec9f450bacb4785f
Anthony <anthony.tram@epitech.eu>
2018-02-28 10:46:48 +0100

    #62 - Added Scripted Coverage on CircleCi

--- tests/coverage_html.sh
+++ tests/coverage_html.sh
@@ -2,6 +2,7 @@
 make fclean && make -j 4
 make -j 4 tests_run
 ./tests/functionals_tests.sh $1
-gcovr -r . --exclude-directories=include/ --delete -s > coverage.log
+gcovr -r . --html -o coverage.html --html-details --exclude-directories=include/ --delete -s
+firefox coverage.html
 find -type f \( -name "*~" -o -name "#*#" -o -name "*.gcno" -o -name "*.gc*" -o -name "*.html" -o  -name "*.o" \) -delete
 make fclean
